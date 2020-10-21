225b2b9178e2264bacf69643a0241cbec6b26402
Nulllink <alexanveld@gmail.com>
2020-10-21 11:51:52 +0300

    Initial commit

---
+++ Collapse/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Collapse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(39, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(473, 393);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "2";
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 448);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(565, 25);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(17, 20);
+            this.toolStripStatusLabel1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Игроки 1-6";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Поле Х";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(319, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Поле Y";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(223, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "8";
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(381, 6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(69, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "8";
+            this.textBox3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 473);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Collapse/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Collapse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool steps = false;
+        int player=0;
+        int maxplayer = 2;
+        int cells1 = 8;
+        int cells2 = 8;
+        List<Color> col = new List<Color>() { Color.Green,Color.Blue,Color.Red,Color.Yellow,Color.Violet,Color.Black};
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            spacecreate();
+        }
+        private void pic_Click(object sender, EventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            if (steps == false)
+            {
+                if (pic.Image == null)
+                {
+                    pic.Image = Properties.Resources.C1;
+                    pic.Image.Tag = player + " " + 1;
+                    pic.BackColor = col[player];
+               
+                    //steps[player] = steps[player] + 1;
+                    if (player < maxplayer - 1)
+                    {
+                        player++;
+                    }
+                    else
+                    {
+                        steps = true;
+                        player = 0;
+                    }
+                    toolStripStatusLabel1.Text = (player+1).ToString();
+                }
+
+            }
+            else
+            {
+                if (pic.Image != null)
+                {
+                    bool pl = colap(pic, false);
+                    if (pl == true)
+                    {
+                        if (player < maxplayer - 1)
+                        {
+                            player++;
+                        }
+                        else
+                        {
+                            player = 0;
+                        }
+                        toolStripStatusLabel1.Text = (player+1).ToString();
+                    }
+                }
+
+            }
+        }
+
+        private bool colap(PictureBox pic,bool rec)
+        {
+            if (pic.Image != null)
+            {
+                string[] tags = pic.Image.Tag.ToString().Split(' ');
+                if (tags[0] == player.ToString()||rec==true)
+                {
+                    if (tags[1] == "1")
+                    {
+                        pic.Image = Properties.Resources.C2;
+                        pic.Image.Tag = player + " " + 2;
+                    }
+                    if (tags[1] == "2")
+                    {
+                        pic.Image = Properties.Resources.C3;
+                        pic.Image.Tag = player + " " + 3;
+                    }
+                    if (tags[1] == "3")
+                    {
+                        pic.Image = Properties.Resources.C4;
+                        pic.Image.Tag = player + " " + 4;
+                        colap4();
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void colap4()
+        {
+            int x, y;
+            int i,for1,for2;
+            int count4=1;
+            while (count4 != 0)
+            {
+                count4 = 0;
+                for (for1 = 1; for1 <= cells1; for1++)
+                    for (for2 = 1; for2 <= cells2; for2++)
+                    {
+                        
+                        PictureBox pic = (PictureBox)panel1.Controls[for1.ToString() + for2.ToString()];
+                        if (pic.Image != null)
+                        {
+                            x = ((Point)pic.Tag).X;
+                            y = ((Point)pic.Tag).Y;
+                            string[] tags = pic.Image.Tag.ToString().Split(' ');
+                            if (tags[1] == "4")
+                            {
+                                pic.Image = null;
+                                pic.BackColor = Color.White;
+                                for (i = 0; i < 4; i++)
+                                {
+                                    PictureBox pic1 = null;
+                                    switch (i)
+                                    {
+                                        case 0:
+                                            if (y - 1 > 0)
+                                            {
+                                                pic1 = (PictureBox)panel1.Controls[x.ToString() + (y - 1).ToString()];
+                                            }
+                                            break;
+                                        case 1:
+                                            if (x + 1 <= cells1)
+                                            {
+                                                pic1 = (PictureBox)panel1.Controls[(x + 1).ToString() + y.ToString()];
+                                            }
+                                            break;
+                                        case 2:
+                                            if (y + 1 <= cells2)
+                                            {
+                                                pic1 = (PictureBox)panel1.Controls[x.ToString() + (y + 1).ToString()];
+                                            }
+                                            break;
+                                        case 3:
+                                            if (x - 1 > 0)
+                                            {
+                                                pic1 = (PictureBox)panel1.Controls[(x - 1).ToString() + y.ToString()];
+                                            }
+                                            break;
+                                    }
+                                    if (pic1 != null)
+                                    {
+                                        if (pic1.Image != null)
+                                        {
+                                            string[] tags1 = pic1.Image.Tag.ToString().Split(' ');
+                                            if (tags1[1] == "1")
+                                            {
+                                                pic1.Image = Properties.Resources.C2;
+                                                pic1.Image.Tag = player + " " + 2;
+                                                pic1.BackColor = col[player];
+                                            }
+                                            if (tags1[1] == "2")
+                                            {
+                                                pic1.Image = Properties.Resources.C3;
+                                                pic1.Image.Tag = player + " " + 3;
+                                                pic1.BackColor = col[player];
+                                            }
+                                            if (tags1[1] == "3")
+                                            {
+                                                pic1.Image = Properties.Resources.C4;
+                                                pic1.Image.Tag = player + " " + 4;
+                                                pic1.BackColor = col[player];
+                                                count4++;
+                                                //colap(pic1,true);
+                                                //textBox1.Text = "5";
+                                            }
+                                        }
+                                        else
+                                        {
+                                            pic1.Image = Properties.Resources.C1;
+                                            pic1.Image.Tag = player + " " + 1;
+                                            pic1.BackColor = col[player];
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+
+        private void spacecreate()
+        {
+            int for1, for2;
+            panel1.Controls.Clear();
+            Text = "Checkers";
+            int w = panel1.Width / cells1;
+            int h = panel1.Height / cells2;
+            for (for1 = 1; for1 <= cells1; for1++)
+                for (for2 = 1; for2 <= cells2; for2++)
+                {
+                    PictureBox pic = new PictureBox();
+                    pic.Height = h;
+                    pic.Width = w;
+                    pic.Location = new Point(w * (for1 - 1), h * (for2 - 1));
+                    pic.BackColor = Color.White;
+                    pic.BorderStyle = BorderStyle.FixedSingle;
+                    pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pic.Tag = new Point(for1, for2);
+                    pic.Name = for1.ToString() + for2.ToString();
+                    pic.Click += pic_Click;
+                    panel1.Controls.Add(pic);
+                }
+        }
+        private void paramch()
+        {
+            cells1 = int.Parse(textBox2.Text);
+            cells2 = int.Parse(textBox3.Text);
+            player = 0;
+            panel1.Width = Width - 60;
+            panel1.Height = Height - 110;
+            spacecreate();
+            maxplayer = int.Parse(textBox1.Text);
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter&&steps==false)
+            {
+                paramch();                
+            }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && steps == false)
+            {
+                paramch();
+            }
+        }
+
+        private void textBox3_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && steps == false)
+            {
+                paramch();
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            if (steps == false)
+            {
+                paramch();
+            }
+        }
+    }
+}
